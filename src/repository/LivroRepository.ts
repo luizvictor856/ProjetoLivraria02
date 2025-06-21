c6b8d527d1fcb5c866b047977a6e8f2c19b7b818
luizvictor856 <luizvictor856@gmail.com>
2025-06-21 18:47:37 -0300

    implementando o livroRepository

---
+++ src/repository/LivroRepository.ts
@@ -0,0 +1,40 @@
+import { Livro } from "../model/livro";
+
+export class LivroRepository {
+    private livros: Livro [] = [];
+
+
+    cadastrar (livro: Livro): void {
+        this.livros.push(livro);
+    }
+
+    buscarISBM(ISBM: string): Livro | undefined {
+        return this.livros.find(livro => livro.ISBN === ISBM);
+    }
+
+    listar(): Livro [] {
+        return this.livros
+    } 
+
+     atualizar(ISBN: string, titulo?: string, autor?: string, editora?: string, edicao?: string, categoria_id?: number): boolean {
+        const livro = this.buscarISBM(ISBN);
+        if (!livro) return false;
+
+        if (titulo !== undefined) livro.titulo = titulo;
+        if (autor !== undefined) livro.autor = autor;
+        if (editora !== undefined) livro.editora = editora;
+        if (edicao !== undefined) livro.edicao = edicao;
+        if (categoria_id !== undefined) livro.categoria_id = categoria_id;
+
+        return true;
+    }
+
+
+    remover(ISBN: string): boolean {
+        const index = this.livros.findIndex(livro => livro.ISBN === ISBN);
+        if (index === -1) return false;
+
+        this.livros.splice(index, 1);
+        return true;
+    }
+}
