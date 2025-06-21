e661ac57ae21bf47e6564dc7e4f0e52c747d322d
luizvictor856 <luizvictor856@gmail.com>
2025-06-21 10:56:36 -0300

    Implementando codigos "das categorias e curso" no model

---
+++ src/model/categoriaLivro.ts
@@ -0,0 +1,3 @@
+export class categotiaLivro {
+    constructor(public id: number, public name: string) {}
+}
---
+++ src/model/categoriaUsuario.ts
@@ -0,0 +1,3 @@
+export class categoriaUsuario {
+    constructor (public id: number, public name: string) {}
+}
