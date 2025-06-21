a31ee21119c401cc32631480b2c77e64d0877cbb
luizvictor856 <luizvictor856@gmail.com>
2025-06-21 18:16:50 -0300

    Implementando UsuarioRepository

---
+++ src/repository/UsuarioRepository.ts
@@ -0,0 +1,54 @@
+import { Usuario } from "../model/usuario";
+
+// utilização do estilo singleton;
+export class UsuarioRepository {
+ 
+    private static instancia: UsuarioRepository;
+    private usuarios: Usuario [] =[];
+
+    private constructor () {}
+
+    static getInstancia(): UsuarioRepository {
+        if (!UsuarioRepository.instancia){
+            UsuarioRepository.instancia = new UsuarioRepository();
+        } 
+        return UsuarioRepository.instancia;
+    }
+
+    cadastrar(usuario: Usuario): void {
+        this.usuarios.push(usuario);
+    }
+
+    buscarCPF(cpf: string): Usuario | undefined {
+        return this.usuarios.find(user => user.cpf === cpf);
+    }
+    
+    atualizar(cpf: string, nome?: string, ativo?: boolean, categoria_id?: number, curso_id?: number, suspensao?: Date): boolean {
+    const usuario = this.buscarCPF(cpf);
+    if (!usuario) return false;
+
+    if (nome !== undefined) usuario.nome = nome;
+    if (ativo !== undefined) usuario.ativo = ativo;
+    if (categoria_id !== undefined) usuario.categoria_id = categoria_id;
+    if (curso_id !== undefined) usuario.curso_id = curso_id;
+    if (suspensao !== undefined) usuario.suspensao = suspensao;
+
+    return true;
+  }
+
+  
+  remover(cpf: string): boolean {
+    const index = this.usuarios.findIndex(user => user.cpf === cpf);
+    if (index === -1) return false;
+
+    this.usuarios.splice(index, 1);
+    return true;
+  }
+
+
+
+
+
+
+
+}
