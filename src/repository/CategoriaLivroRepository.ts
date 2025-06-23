9b989b8ad74d1446320d4d16e1ca5a784acfd389
luizvictor856 <luizvictor856@gmail.com>
2025-06-23 20:45:23 -0300

    implementando categorrias do repository

---
+++ src/repository/CategoriaLivroRepository.ts
@@ -0,0 +1,32 @@
+import { CategoriaLivro } from "../model/categoriaLivro";
+
+export class CategoriaLivroRepository {
+    private static instance: CategoriaLivroRepository;
+    private categorias: CategoriaLivro [] = [
+        new CategoriaLivro (1, "Romance"),
+        new CategoriaLivro (2, "Computação"),
+        new CategoriaLivro (3, "Letras"),
+        new CategoriaLivro (4, "Gestão"),
+    ]
+
+    private constructor () {}
+
+    public static getInstance(): CategoriaLivroRepository {
+
+        if (!this.instance) {
+            this.instance = new CategoriaLivroRepository
+        }
+        return this.instance;
+
+   
+    }
+
+     listarCategorias(): CategoriaLivro [] {
+        return this.categorias
+     }
+
+
+     buscarPorID(id:number): CategoriaLivro | undefined {
+        return this.categorias.find(cat => cat.id === id);
+     } 
+}
